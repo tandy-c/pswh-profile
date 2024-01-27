0ee834d4a0e677accf57103bc5f55748d1fffacf
Johan Cho <cho.joh@northeastern.edu>
2024-01-27 00:05:04 -0500

    add sqliterc file

--- profile.ps1
+++ profile.ps1
@@ -398,6 +398,25 @@ Function isadmin {
     $isAdmin
 }
 
+function Open-Sqlite {
+    <#
+    .SYNOPSIS
+        Initializes sqlite.
+    .DESCRIPTION
+        Initializes sqlite. For example, Init-Sqlite initializes sqlite.
+    .EXAMPLE
+        Init-Sqlite
+    .OUTPUTS
+        System.null
+    #>
+    $sqlite = Get-First-Path-If-Exists "$env:Programfiles\WinGet\Links\sqlite3.exe" "$env:Programfiles (x86)\WinGet\Links\sqlite3.exe"
+    if (!($sqlite)) {
+        Write-Output "Sqlite not found." -Fore Red
+        return
+    }
+    Start-Process $sqlite -ArgumentList "-init $(Join-Path (Split-Path -Parent $profile.CurrentUserAllHosts) '.sqliterc')", "$args" -NoNewWindow
+}
+
 
 function _Main {
     <#
@@ -437,7 +456,9 @@ function _Main {
         "n"               = "$env:windir\notepad.exe";
         "np"              = "$env:windir\notepad.exe";
         "python3"         = "$env:Programfiles\Python312\python.exe";
-        "sqlite"          = "$env:Programfiles\WinGet\Links\sqlite3.exe";   
+        # "sqlite"          = "$env:Programfiles\WinGet\Links\sqlite3.exe";
+        "sqlite"          = "Open-Sqlite";
+        "sqlite3"         = "Open-Sqlite";
         "whereis"         = "Get-CommandPath";
         "open"            = "Invoke-Item";
         "run"             = "Invoke-Item";
