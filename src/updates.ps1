205aec81adac2a16142db441ac8b342280504896
Johan Cho <jcho@hks.harvard.edu>
2024-02-28 16:00:09 -0500

    remove unecessary else after return

--- src/updates.ps1
+++ src/updates.ps1
@@ -51,33 +51,32 @@ function Update-Drivers([switch] $restart, [switch] $force) {
     $Updates = $SearchResult.Updates
     if ([string]::IsNullOrEmpty($Updates)) {
         Write-Host "No pending driver updates."
+        return
     }
-    else {
-        #Show available Drivers...
-        $Updates | Select-Object Title, DriverModel, DriverVerDate, Driverclass, DriverManufacturer | Format-List
-        $UpdatesToDownload = New-Object -Com Microsoft.Update.UpdateColl
-        $updates | ForEach-Object { $UpdatesToDownload.Add($_) | out-null }
-        Write-Host('Downloading Drivers...')  -Fore Green
-        $UpdateSession = New-Object -Com Microsoft.Update.Session
-        $Downloader = $UpdateSession.CreateUpdateDownloader()
-        $Downloader.Updates = $UpdatesToDownload
-        $Downloader.Download()
-        $UpdatesToInstall = New-Object -Com Microsoft.Update.UpdateColl
-        $updates | ForEach-Object { if ($_.IsDownloaded) { $UpdatesToInstall.Add($_) | out-null } }
+    #Show available Drivers...
+    $Updates | Select-Object Title, DriverModel, DriverVerDate, Driverclass, DriverManufacturer | Format-List
+    $UpdatesToDownload = New-Object -Com Microsoft.Update.UpdateColl
+    $updates | ForEach-Object { $UpdatesToDownload.Add($_) | out-null }
+    Write-Host('Downloading Drivers...')  -Fore Green
+    $UpdateSession = New-Object -Com Microsoft.Update.Session
+    $Downloader = $UpdateSession.CreateUpdateDownloader()
+    $Downloader.Updates = $UpdatesToDownload
+    $Downloader.Download()
+    $UpdatesToInstall = New-Object -Com Microsoft.Update.UpdateColl
+    $updates | ForEach-Object { if ($_.IsDownloaded) { $UpdatesToInstall.Add($_) | out-null } }
 
-        Write-Host('Installing Drivers...')  -Fore Green
-        $Installer = $UpdateSession.CreateUpdateInstaller()
-        $Installer.Updates = $UpdatesToInstall
-        $InstallationResult = $Installer.Install()
-        if ($InstallationResult.RebootRequired) { 
-            Write-Host('Reboot required! Please reboot now.') -Fore Red
-            if ($restart -or $force) {
-                Restart-Computer
-            }
+    Write-Host('Installing Drivers...')  -Fore Green
+    $Installer = $UpdateSession.CreateUpdateInstaller()
+    $Installer.Updates = $UpdatesToInstall
+    $InstallationResult = $Installer.Install()
+    if ($InstallationResult.RebootRequired) { 
+        Write-Host('Reboot required! Please reboot now.') -Fore Red
+        if ($restart -or $force) {
+            Restart-Computer
         }
-        else { Write-Host('Done.') -Fore Green }
-        $updateSvc.Services | Where-Object { $_.IsDefaultAUService -eq $false -and $_.ServiceID -eq "7971f918-a847-4430-9279-4a52d1efe18d" } | ForEach-Object { $UpdateSvc.RemoveService($_.ServiceID) }
     }
+    else { Write-Host('Done.') -Fore Green }
+    $updateSvc.Services | Where-Object { $_.IsDefaultAUService -eq $false -and $_.ServiceID -eq "7971f918-a847-4430-9279-4a52d1efe18d" } | ForEach-Object { $UpdateSvc.RemoveService($_.ServiceID) }
 }
 
 
